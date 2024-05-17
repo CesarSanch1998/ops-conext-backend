372cce17bea3bcdc0d21ac38e15779a6006c1407
CesarSanch1998 <cs96548@gmail.com>
2024-05-17 12:59:46 -0400

    route

--- routes/update_db.py
+++ routes/update_db.py
@@ -10,11 +10,11 @@ from dotenv import load_dotenv
 load_dotenv()
 update_db = APIRouter()
 
-@update_db.get("/update_client_db_status")
+@update_db.get("/update-client-db-status")
 def status():
     return HTTPException(status_code=202, detail="UPDB_Running")
 
-@update_db.post("/update_client_db")
+@update_db.post("/update-client-db")
 def update_client_db(data: Data_resync_request):
     # Api key smartolt ----------------------
     if data.api_key != os.environ["API_KEY"]:
